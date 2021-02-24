84cc729e3ac34d339ef72fcd56eeb00fb19353b4
User <User@WIN-5RSC18PG333>
2021-02-24 14:29:55 +0500

    Добавьте файлы проекта.

---
+++ MyProjection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyProjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ MyProjection/folder/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProjection.folder
+{
+    class Transaction
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public  decimal Amount { get; set; }
+        public DateTime DateofTransaction { get; set; }
+           
+
+    }
+}
